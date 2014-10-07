eb3da2951593f6949413e675ec537304446e7193
knu2xs <knu2xs@gmail.com>
2014-10-07 19:39:11 -0400

    add putin feature layer and scaffold basemap dropdown

--- app/js/map.js
+++ app/js/map.js
@@ -1,11 +1,16 @@
 require(["esri/map",
         "application/bootstrapmap",
+        "esri/layers/FeatureLayer",
         "dojo/domReady!"],
-    function (Map, BootstrapMap) {
+    function (Map, BootstrapMap, FeatureLayer) {
         <!-- Get a reference to the ArcGIS Map class -->
         var map = BootstrapMap.create("mapDiv", {
             basemap: "national-geographic",
-            center: [-122.6819, 45.5200],
+            center: [-122.88149, 47.04299],
             zoom: 10
         });
+        var featureLayerPutIn = new FeatureLayer(
+            'http://services.arcgis.com/SgB3dZDkkUxpEHxu/ArcGIS/rest/services/whitewaterPutin/FeatureServer/0'
+        );
+        map.addLayer(featureLayerPutIn);
     });
