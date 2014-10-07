a27cb01293d570331522008d3e4213ab9081b388
knu2xs <knu2xs@gmail.com>
2014-10-07 19:49:42 -0400

    change to gray basemap and add dual scalebar

--- app/js/map.js
+++ app/js/map.js
@@ -1,14 +1,25 @@
 require(["esri/map",
         "application/bootstrapmap",
+        "esri/dijit/Scalebar",
         "esri/layers/FeatureLayer",
         "dojo/domReady!"],
-    function (Map, BootstrapMap, FeatureLayer) {
-        <!-- Get a reference to the ArcGIS Map class -->
+    function (Map, BootstrapMap, Scalebar, FeatureLayer) {
+
+        // create map object centered on Olympia, WA
         var map = BootstrapMap.create("mapDiv", {
-            basemap: "national-geographic",
+            basemap: "gray",
             center: [-122.88149, 47.04299],
             zoom: 10
         });
+
+        // add a scalebar to the map
+        new Scalebar({
+            attachTo: 'bottom-left',
+            map: map,
+            scalebarUnit: 'dual'
+        });
+
+        // add the putin feature layer to the map
         var featureLayerPutIn = new FeatureLayer(
             'http://services.arcgis.com/SgB3dZDkkUxpEHxu/ArcGIS/rest/services/whitewaterPutin/FeatureServer/0'
         );
